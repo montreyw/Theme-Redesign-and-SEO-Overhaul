8fdd11f41357d6d9e5addae7dc0d40c9ec34a4dd
Andre Bulatov <andre.bulatov@gmail.com>
2016-01-22 22:40:42 -0500

    New MP3 + self-hosted CSS + improved timing for new MP3

--- wp-content/themes/mag-wp/js/custom.js
+++ wp-content/themes/mag-wp/js/custom.js
@@ -95,11 +95,11 @@ jQuery( document ).ready( function( $ ) {
 		function y(){if(window.pageYOffset){return window.pageYOffset}
 		return Math.max(document.documentElement.scrollTop,document.body.scrollTop)}
 		function E(e){var t=m(e);return t>=w&&t<=b+w}
-		function S(){var e=document.createElement("audio");e.setAttribute("class",l);e.src=i;e.loop=false;e.addEventListener("canplay",function(){setTimeout(function(){x(k)},500);setTimeout(function(){N();p();for(var e=0;e<O.length;e++){T(O[e])}},15500)},true);e.addEventListener("ended",function(){N();h()},true);e.innerHTML=" <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";document.body.appendChild(e);e.play()}
+		function S(){var e=document.createElement("audio");e.setAttribute("class",l);e.src=i;e.loop=false;e.addEventListener("canplay",function(){setTimeout(function(){x(k)},500);setTimeout(function(){N();p();for(var e=0;e<O.length;e++){T(O[e])}},14850)},true);e.addEventListener("ended",function(){N();h()},true);e.innerHTML=" <p>If you are reading this, it is because your browser does not support the audio element. We recommend that you get a new browser.</p> <p>";document.body.appendChild(e);e.play()}
 		function x(e){e.className+=" "+s+" "+o}
 		function T(e){e.className+=" "+s+" "+u[Math.floor(Math.random()*u.length)]}
 		function N(){var e=document.getElementsByClassName(s);var t=new RegExp("\\b"+s+"\\b");for(var n=0;n<e.length;){e[n].className=e[n].className.replace(t,"")}}
-		var e=30;var t=30;var n=350;var r=350;var i="//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake.mp3";var s="mw-harlem_shake_me";var o="im_first";var u=["im_drunk","im_baked","im_trippin","im_blown"];var a="mw-strobe_light";var f="//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css";var l="mw_added_css";var b=g();var w=y();var C=document.getElementsByTagName("*");var k=null;for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){if(E(A)){k=A;break}}}
+		var e=30;var t=30;var n=350;var r=350;var i="/wp-content/uploads/earmillk-harlem-shake/Harlem-Shake-DjSliink-Remix-Edited.mp3";var s="mw-harlem_shake_me";var o="im_first";var u=["im_drunk","im_baked","im_trippin","im_blown"];var a="mw-strobe_light";var f="/wp-content/uploads/earmillk-harlem-shake/earmilk-harlem-shake.css";var l="mw_added_css";var b=g();var w=y();var C=document.getElementsByTagName("*");var k=null;for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){if(E(A)){k=A;break}}}
 		if(A===null){console.warn("Could not find a node of the right size. Please try a different page.");return}
 		c();S();var O=[];for(var L=0;L<C.length;L++){var A=C[L];if(v(A)){O.push(A)}}})()
 	}
