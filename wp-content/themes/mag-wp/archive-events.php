25f2a8923245ebdbc4362300f7e790b41a723753
Andre Bulatov <andre.bulatov@gmail.com>
2016-02-13 03:41:45 -0500

    Custom template for EVENTS Posts plugin
    If this customization is desired, it must be enabled in the `Events
    Plugin Settings > Display > Basic Template Settings > Event Template`

---
+++ wp-content/themes/mag-wp/archive-events.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Template - EVENTS Archive List Page
+*/
+?>
+<?php get_header(); // add header ?>
+<!-- Begin Content - Template - EVENTS Archive List Page archive-events.php -->
+<div class="wrap-fullwidth">
+	<div class="single-content">
+		<div class="entry">
+			<?php if (have_posts()) : while (have_posts()) : the_post();  ?>
+				<?php the_content(''); // content ?>
+			<?php endwhile; endif; ?>
+		</div>
+<!--
+		<article>
+				<div class="<?php echo andre_get_post_class_without_hentry(); ?>" id="post-<?php the_ID(); ?>">
+						<div class="p-first-letter entry-content">
+						</div>
+						<?php wp_link_pages(); // content pagination ?>
+						<div class="clear"></div><br />
+				</div>
+		</article>
+-->
+	</div><!-- end .single-content -->
+	<!-- Begin Sidebar (right) -->
+	<?php  get_sidebar(); // add sidebar ?>
+	<!-- end #sidebar  (right) -->
+	<div class="clear"></div>
+</div><!-- end .wrap-fullwidth -->
+<?php get_footer(); // add footer  ?>
