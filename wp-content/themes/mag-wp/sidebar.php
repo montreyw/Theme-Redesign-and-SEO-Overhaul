8cf705bf2aebc25016b799bfa3a06d338e7fdf8b
Andre Bulatov <andre.bulatov@gmail.com>
2016-02-13 02:29:26 -0500

    Added WPSideBar Schema to all pages (I think)

--- wp-content/themes/mag-wp/sidebar.php
+++ wp-content/themes/mag-wp/sidebar.php
@@ -1,3 +1,6 @@
-<aside class="sidebar">
+<aside class="sidebar" 
+	itemprop="hasPart" itemscope itemtype="http://schema.org/WPSideBar"> 
+	<meta itemprop="name" content="EARMILK Site Sidebar" />
+	<meta itemprop="description" content="This is the main sidebar for EARMILK.com" />
 <?php if ( ! dynamic_sidebar( 'sidebar' ) ) : ?><?php endif; ?>
 </aside>
--- wp-content/themes/mag-wp/sidebar2.php
+++ wp-content/themes/mag-wp/sidebar2.php
@@ -1,3 +1,6 @@
-<aside class="sidebar-middle">
+<aside class="sidebar-middle" 
+	itemprop="hasPart" itemscope itemtype="http://schema.org/WPSideBar"> 
+	<meta itemprop="name" content="EARMILK Site Sidebar" />
+	<meta itemprop="description" content="This is the middle sidebar for EARMILK.com" />
 <?php if ( ! dynamic_sidebar( 'sidebar-middle' ) ) : ?><?php endif; ?>
 </aside>
