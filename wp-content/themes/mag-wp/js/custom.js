67235e8694c68d170e0bb6d925b55ae80ce214af
Andre Bulatov <andre.bulatov@gmail.com>
2016-02-20 00:21:53 -0500

    Type Anywhere Search JS - custom.js

--- wp-content/themes/mag-wp/js/custom.js
+++ wp-content/themes/mag-wp/js/custom.js
@@ -154,6 +154,60 @@ jQuery( document ).ready( function( $ ) {
 	}
 
 
+
+	//////////////////////////////////////////////////////////////////
+	// Type Anywhere MySpace-like Search - Andre
+	//////////////////////////////////////////////////////////////////
+	// global keycode constant
+	var KEYCODE_ESC = 27;
+	// extending the jQuery prototype with setCursorPosition
+	$.fn.setCursorPosition = function(pos) {
+		this.each(function(index, elem) {
+			if (elem.setSelectionRange) {
+				elem.setSelectionRange(pos, pos);
+			} else if (elem.createTextRange) {
+				var range = elem.createTextRange();
+				range.collapse(true);
+				range.moveEnd('character', pos);
+				range.moveStart('character', pos);
+				range.select();
+			}
+		});
+		return this;
+	};
+	// intialize
+	$(document).ready( function() {
+	// cache variables
+		var $search = $('#ta-search');
+		var $searchtext = $('#ta-searchtext');
+		// on any keydown, start parsing keyboard input
+		$(document).keydown(function(e) {
+			if ( !$("*:not(#ta-searchtext)").is(":focus") ) {
+				if($search.is(':visible')) {
+					switch (e.which) {
+						case KEYCODE_ESC:
+							$search.fadeOut(200);
+							$searchtext.blur().hide();
+							break;
+						default:
+							$searchtext.focus();
+							break;
+					}
+				} else {
+					$searchtext.show().focus();
+					// grab the key pressed ( String.fromCharCode(e.which) ) 
+					// and insert it into $searchtext.
+					// then,set the cursor to the end of $searchtext.
+					$searchtext.val(String.fromCharCode(e).toLowerCase())
+						.setCursorPosition($searchtext.val().length);
+					$search.fadeIn(200); 
+				}
+			}
+		});
+	});
+
+
+
 	//////////////////////////////////////////////////////////////////
 	// Style the first word of Interview Quetions and Answers - Andre
 	//////////////////////////////////////////////////////////////////
