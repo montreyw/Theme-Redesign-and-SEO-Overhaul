a719a2207ce8e677c320630723f1f1ba54a09644
Andre Bulatov <andre.bulatov@gmail.com>
2016-02-23 07:52:24 -0500

    Type Anywhere Search Escape kbd key HTMl added + ALEXA certification code

--- wp-content/themes/mag-wp/header.php
+++ wp-content/themes/mag-wp/header.php
@@ -28,6 +28,13 @@
         x.parentNode.insertBefore(s, x);
      })();
 </script>
+<!-- Start Alexa Certify Javascript -->
+<script type="text/javascript">
+_atrk_opts = { atrk_acct:"PBGOm1aMp4Z3Y8", domain:"earmilk.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+</script>
+<noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=PBGOm1aMp4Z3Y8" style="display:none" height="1" width="1" alt="" /></noscript>
+<!-- End Alexa Certify Javascript -->  
 <?php
     // Options from admin panel
     global $smof_data;
@@ -186,7 +193,7 @@
 	itemscope itemtype="http://schema.org/<?php 
 	if (is_page(array(248463, 'about', 'About'))) { 
 		echo 'AboutPage'; 
-	} elseif (is_author()) {
+	} elseif ( ( is_author() ) || ( is_page_template( 'archive-album_review.php' ) ) ) {
 		echo 'CollectionPage';
 	} elseif (is_page(array(248509, 'contact-us', 'Contact Us'))) {
 		echo 'ContactPage'; 
@@ -256,6 +263,7 @@
 
 <div id="ta-search-container">
 	<div id="ta-search">
+		<span id="esc-to-exit">Press <kbd id="escape-key">Esc</kbd> key to exit.</span>
 		<form id="ta-searchform" method="get" action="http://earmilk.com/">
 			<label id="ta-label" for="ta-searchtext">search: </label>
 			<input id="ta-searchtext" name="s" type="text" />
