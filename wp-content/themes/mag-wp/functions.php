bd457a78cf546a34a575f25a700d8c1e1cbda09d
Andre Bulatov <andre.bulatov@gmail.com>
2016-03-02 19:02:59 -0500

    functions.php -- schema breadcrumbs function

--- wp-content/themes/mag-wp/functions.php
+++ wp-content/themes/mag-wp/functions.php
@@ -663,3 +663,241 @@ function andre_get_iframe_src( $input ) {
 		$return = $output[1];
 	return $return;
 }
+
+// ---------------------------------------------------------------------------------------------------- 
+// Breadcrumbs with Schema - Andre
+// ---------------------------------------------------------------------------------------------------- 
+function custom_breadcrumbs() {
+       
+    // Settings
+    $separator          = '&gt;';
+    $breadcrums_id      = 'breadcrumbs';
+    $breadcrums_class   = 'breadcrumbs';
+    $home_title         = 'Homepage';
+    $prefix = '';
+    $current_uri = home_url( add_query_arg( NULL, NULL ) );
+      
+    // If you have any custom post types with custom taxonomies, put the taxonomy name below (e.g. product_cat)
+    $custom_taxonomy    = 'post, opinion_post, gear_post, album_review, news';
+       
+    // Get the query & post information
+    global $post,$wp_query;
+       
+    // Do not display on the homepage
+    if ( !is_front_page() ) {
+       
+        // Build the breadcrums
+        echo '<ul id="' . $breadcrums_id . '" class="' . $breadcrums_class . '" itemprop="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
+           
+        // Home page
+        echo '<li class="item-home" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-link bread-home" href="' . get_home_url() . '" title="' . $home_title . '" itemprop="item"><span itemprop="name">' . $home_title . '</span></a></li>';
+        echo '<li class="separator separator-home"> ' . $separator . ' </li>';
+           
+        if ( is_archive() && !is_tax() && !is_category() && !is_tag() && !is_month() && !is_year() ) {
+              
+            echo '<li class="item-current item-archive" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong><a href="' . get_post_type_archive_link( get_post_type( get_the_ID() ) ) . '" class="bread-current bread-archive" itemprop="item"><span itemprop="name">' . post_type_archive_title($prefix, false) . '<span></a><strong></li>';
+              
+        } else if ( is_archive() && is_tax() && !is_category() && !is_tag() && !is_month() && !is_year() ) {
+              
+            // If post is a custom post type
+            $post_type = get_post_type();
+              
+            // If it is a custom post type display name and link
+            if($post_type != 'post') {
+                  
+                $post_type_object = get_post_type_object($post_type);
+                $post_type_archive = get_post_type_archive_link($post_type);
+              
+                echo '<li class="item-cat item-custom-post-type-' . $post_type . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-cat bread-custom-post-type-' . $post_type . '" href="' . $post_type_archive . '" title="' . $post_type_object->labels->name . '" itemprop="item"><span itemprop="name">' . $post_type_object->labels->name . '</span></a></li>';
+                echo '<li class="separator"> ' . $separator . ' </li>';
+              
+            }
+              
+            $custom_tax_name = get_queried_object()->name;
+            echo '<li class="item-current item-archive" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-archive"><span itemprop="name">' . $custom_tax_name . '</span></strong></li>';
+              
+        } else if ( is_single() ) {
+              
+            // If post is a custom post type
+            $post_type = get_post_type();
+              
+            // If it is a custom post type display name and link
+            if($post_type != 'post') {
+                  
+                $post_type_object = get_post_type_object($post_type);
+                $post_type_archive = get_post_type_archive_link($post_type);
+              
+                echo '<li class="item-cat item-custom-post-type-' . $post_type . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-cat bread-custom-post-type-' . $post_type . '" href="' . $post_type_archive . '" title="' . $post_type_object->labels->name . '" itemprop="item"><span itemprop="name">' . $post_type_object->labels->name . '</span></a></li>';
+                echo '<li class="separator"> ' . $separator . ' </li>';
+              
+            }
+              
+            // Get post category info
+            $category = get_the_category();
+             
+            if(!empty($category)) {
+              
+                // Get last category post is in
+                $arr_vals = array_values($category);
+                $last_category = end($arr_vals);
+                  
+                // Get parent any categories and create array
+                $get_cat_parents = rtrim(get_category_parents($last_category->term_id, false, ','),',');
+                $cat_parents = explode(',',$get_cat_parents);
+                  
+                // Loop through parent categories and store in variable $cat_display
+                $cat_display = '';
+                foreach($cat_parents as $parents) {
+                    $cat_display .= '<li class="item-cat" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' . get_category_link( $last_category->term_id ) . '" itemprop="item"><span itemprop="name">'.$parents.'</span></a></li>';
+                    $cat_display .= '<li class="separator"> ' . $separator . ' </li>';
+                }
+             
+            }
+              
+            // If it's a custom post type within a custom taxonomy
+            $taxonomy_exists = taxonomy_exists($custom_taxonomy);
+            if(empty($last_category) && !empty($custom_taxonomy) && $taxonomy_exists) {
+                   
+                $taxonomy_terms = get_the_terms( $post->ID, $custom_taxonomy );
+                $cat_id         = $taxonomy_terms[0]->term_id;
+                $cat_nicename   = $taxonomy_terms[0]->slug;
+                $cat_link       = get_term_link($taxonomy_terms[0]->term_id, $custom_taxonomy);
+                $cat_name       = $taxonomy_terms[0]->name;
+               
+            }
+              
+            // Check if the post is in a category
+            if(!empty($last_category)) {
+                echo $cat_display;
+                echo '<li class="item-current item-' . $post->ID . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . get_the_title() . '</span></a></strong></li>';
+                  
+            // Else if post is in a custom taxonomy
+            } else if(!empty($cat_id)) {
+                  
+                echo '<li class="item-cat item-cat-' . $cat_id . ' item-cat-' . $cat_nicename . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-cat bread-cat-' . $cat_id . ' bread-cat-' . $cat_nicename . '" href="' . $cat_link . '" title="' . $cat_name . '" itemprop="item"><span itemprop="name">' . $cat_name . '</span></a></li>';
+                echo '<li class="separator"> ' . $separator . ' </li>';
+                echo '<li class="item-current item-' . $post->ID . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . get_the_title() . '</span></a></strong></li>';
+              
+            } else {
+                  
+                echo '<li class="item-current item-' . $post->ID . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-' . $post->ID . '" title="' . get_the_title() . '"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . get_the_title() . '</span></a></strong></li>';
+                  
+            }
+              
+        } else if ( is_category() && !is_archive() ) {
+               
+            // Category page
+            echo '<li class="item-current item-cat"><strong class="bread-current bread-cat" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . single_cat_title('', false) . '</span></a></strong></li>';
+               
+        } else if ( is_category() ) {
+	        
+            // Category page
+            echo '<li class="item-current item-cat"><strong class="bread-current bread-cat" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' . $current_uri . '" itemprop="item"><span itemprop="name">' . single_cat_title('', false) . '</span></a></strong></li>';
+               
+        } else if ( is_page() ) {
+               
+            // Standard page
+            if( $post->post_parent ){
+                   
+                // If child page, get parents 
+                $anc = get_post_ancestors( $post->ID );
+                   
+                // Get parents in the right order
+                $anc = array_reverse($anc);
+                   
+                // Parent page loop
+                foreach ( $anc as $ancestor ) {
+                    $parents .= '<li class="item-parent item-parent-' . $ancestor . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-parent bread-parent-' . $ancestor . '" href="' . get_permalink($ancestor) . '" title="' . get_the_title($ancestor) . '"> itemprop="item"><span itemprop="name">' . get_the_title($ancestor) . '</span></a></li>';
+                    $parents .= '<li class="separator separator-' . $ancestor . '"> ' . $separator . ' </li>';
+                }
+                   
+                // Display parent pages
+                echo $parents;
+                   
+                // Current page
+                echo '<li class="item-current item-' . $post->ID . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong title="' . get_the_title() . '"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . get_the_title() . '</span></a></strong></li>';
+                   
+            } else {
+                   
+                // Just display current page if not parents
+                echo '<li class="item-current item-' . $post->ID . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-' . $post->ID . '"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name">' . get_the_title() . '</span></a></strong></li>';
+                   
+            }
+               
+        } else if ( is_tag() ) {
+               
+            // Tag page
+               
+            // Get tag information
+            $term_id        = get_query_var('tag_id');
+            $taxonomy       = 'post_tag';
+            $args           = 'include=' . $term_id;
+            $terms          = get_terms( $taxonomy, $args );
+            $get_term_id    = $terms[0]->term_id;
+            $get_term_slug  = $terms[0]->slug;
+            $get_term_name  = $terms[0]->name;
+               
+            // Display the tag name
+            echo '<li class="item-current item-tag-' . $get_term_id . ' item-tag-' . $get_term_slug . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' . get_permalink() . '" itemprop="item"><span itemprop="name"><strong class="bread-current bread-tag-' . $get_term_id . ' bread-tag-' . $get_term_slug . '">' . $get_term_name . '</span></a></strong></li>';
+           
+        } elseif ( is_day() ) {
+               
+            // Day archive
+               
+            // Year link
+            echo '<li class="item-year item-year-' . get_the_time('Y') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link( get_the_time('Y') ) . '" title="' . get_the_time('Y') . '" itemprop="item"><span itemprop="name">' . get_the_time('Y') . ' Archives</span></a></li>';
+            echo '<li class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </li>';
+               
+            // Month link
+            echo '<li class="item-month item-month-' . get_the_time('F') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-month bread-month-' . get_the_time('F') . '" href="' . get_month_link( get_the_time('Y'), get_the_time('F') ) . '" title="' . get_the_time('F') . '" itemprop="item"><span itemprop="name">' . get_the_time('F') . ' Archives</span></a></li>';
+            echo '<li class="separator separator-' . get_the_time('F') . '"> ' . $separator . ' </li>';
+               
+            // Day display
+            echo '<li class="item-current item-' . get_the_time('j') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-' . get_the_time('j') . '"><a class="bread-year bread-year-' . get_the_time('j') . '" href="' . get_year_link( get_the_time('j') ) . '" title="' . get_the_time('j') . '" itemprop="item"><span itemprop="name">' . get_the_time('jS') . ' ' . get_the_time('F') . ' Archives</span></a></strong></li>';
+               
+        } else if ( is_month() ) {
+               
+            // Month Archive
+               
+            // Year link
+            echo '<li class="item-year item-year-' . get_the_time('Y') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-year bread-year-' . get_the_time('Y') . '" href="' . get_year_link( get_the_time('Y') ) . '" title="' . get_the_time('Y') . '" itemprop="item"><span itemprop="name">' . get_the_time('Y') . ' Archives</span></a></li>';
+            echo '<li class="separator separator-' . get_the_time('Y') . '"> ' . $separator . ' </li>';
+               
+            // Month display
+            echo '<li class="item-month item-month-' . get_the_time('F') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a class="bread-year bread-year-' . get_the_time('F') . '" href="' . get_month_link( get_the_time('Y'), get_the_time('m') ) . '" title="' . get_the_time('F') . '" itemprop="item"><strong class="bread-month bread-month-' . get_the_time('F') . '" title="' . get_the_time('F') . '"><span itemprop="name">' . get_the_time('F') . ' Archives</span></strong></a></li>';
+               
+        } else if ( is_year() ) {
+               
+            // Display year archive
+            echo '<li class="item-current item-current-' . get_the_time('Y') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-current-' . get_the_time('Y') . '" title="' . get_the_time('Y') . '" itemprop="item"><span itemprop="name">' . get_the_time('Y') . ' Archives</span></strong></li>';
+               
+        } else if ( is_author() ) {
+               
+            // Auhor archive
+               
+            // Get the author information
+            global $author;
+            $userdata = get_userdata( $author );
+               
+            // Display author name
+            echo '<li class="item-current item-current-' . $userdata->user_nicename . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-current-' . $userdata->user_nicename . '" title="' . $userdata->display_name . '"><span itemprop="name">' . 'Author: ' . $userdata->display_name . '</span></strong></li>';
+           
+        } else if ( get_query_var('paged') ) {
+               
+            // Paginated archives
+            echo '<li class="item-current item-current-' . get_query_var('paged') . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-current-' . get_query_var('paged') . '" title="Page ' . get_query_var('paged') . '"><span itemprop="name">'.__('Page') . ' ' . get_query_var('paged') . '</span></strong></li>';
+               
+        } else if ( is_search() ) {
+           
+            // Search results page
+            echo '<li class="item-current item-current-' . get_search_query() . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><strong class="bread-current bread-current-' . get_search_query() . '" title="Search results for: ' . get_search_query() . '"><span itemprop="name">Search results for: ' . get_search_query() . '</span></strong></li>';
+           
+        } elseif ( is_404() ) {
+               
+            // 404 page
+            echo '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span itemprop="name">' . 'Error 404' . '</span></li>';
+        }
+       
+        echo '</ul>';
+	}
+}
