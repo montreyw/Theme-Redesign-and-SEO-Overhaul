03b7ebf26303255802685a5fca321c85b5dbca90
Andre Bulatov <andre.bulatov@gmail.com>
2016-03-08 20:15:39 -0500

    Moved `<head>` breaking Alexa certification `<noscript>` tag from header.php to footer.php

--- wp-content/themes/mag-wp/header.php
+++ wp-content/themes/mag-wp/header.php
@@ -28,13 +28,6 @@
         x.parentNode.insertBefore(s, x);
      })();
 </script>
-<!-- Start Alexa Certify Javascript -->
-<script type="text/javascript">
-_atrk_opts = { atrk_acct:"PBGOm1aMp4Z3Y8", domain:"earmilk.com",dynamic: true};
-(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
-</script>
-<noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=PBGOm1aMp4Z3Y8" style="display:none" height="1" width="1" alt="" /></noscript>
-<!-- End Alexa Certify Javascript -->  
 <?php
     // Options from admin panel
     global $smof_data;
